c547c3b3ba3203dd94d556c6fba5527d84546c9c
RichardLimaDxD <richardpires663802@gmail.com>
2025-02-11 20:34:41 -0300

    feat: user entity update - integration test

--- src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
@@ -105,4 +105,29 @@ describe('User entity integration tests', () => {
       expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
     });
   });
+
+  describe('Update method', () => {
+    it('Should throw an error when update a user with invalid name', () => {
+      const entity = new UserEntity(UserDataBuilder({}));
+      expect(() => entity.update(null)).toThrowError(EntityValidationError);
+      expect(() => entity.update('')).toThrowError(EntityValidationError);
+      expect(() => entity.update(10 as any)).toThrowError(
+        EntityValidationError,
+      );
+      expect(() => entity.update('a'.repeat(256))).toThrowError(
+        EntityValidationError,
+      );
+    });
+
+    it('Should a valid update user', () => {
+      expect.assertions(0);
+
+      const props: UserProps = {
+        ...UserDataBuilder({}),
+      };
+
+      const entity = new UserEntity(props);
+      entity.update('other name');
+    });
+  });
 });
