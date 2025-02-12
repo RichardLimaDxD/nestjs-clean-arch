e8494809715984eb7a592b076b599574986828f3
RichardLimaDxD <richardpires663802@gmail.com>
2025-02-11 21:57:07 -0300

    feat: adding repository in memory and not found error

--- src/shared/domain/repositories/repository-contracts.ts
+++ src/shared/domain/repositories/repository-contracts.ts
@@ -2,8 +2,8 @@ import { Entity } from '../entities/entity';
 
 export interface RepositoryInterface<E extends Entity> {
   insert(entity: E): Promise<void>;
-  findById(id: string): Promise<E>;
   findAll(): Promise<E[]>;
+  findById(id: string): Promise<E>;
   update(entity: E): Promise<void>;
   delete(id: string): Promise<void>;
 }
